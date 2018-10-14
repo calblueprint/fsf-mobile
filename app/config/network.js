585f053848f475199176ccfc4eea09825ab18b77
aivantg <aivant.developer@gmail.com>
2018-10-14 14:24:53 -0700

    added network settings

---
+++ app/config/network.js
@@ -0,0 +1,23 @@
+import { Platform } from 'react-native'
+
+/**
+ * Determines URL based on whether in production or dev environment
+ */
+if (process.env.NODE_ENV === 'production') {
+  console.log('In Production Environment');
+  URL = 'https://fsfmobile.fsf.org.com'
+} else {
+  console.log('In Staging (Dev) Environment');
+  URL = Platform.select({
+    // You'll need to change this to your computer's IP/ngrok if using mobile simulator
+    ios: "http://localhost:3000",
+    android: "http://10.0.2.2:3000"
+  });
+}
+
+export const networkSettings = {
+  env: process.env.NODE_ENV,
+  URL,
+};
+
+export default networkSettings;
