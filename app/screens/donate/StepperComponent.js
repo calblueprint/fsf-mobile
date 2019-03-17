3e8e491716d09c72c65472c407568cb0454a9ef7
Tony Yang <tonyyanga@gmail.com>
2019-03-17 11:54:03 -0700

    remove stepper due to @expo conflict

--- app/screens/donate/StepperComponent.js
+++ app/screens/donate/StepperComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Text, View, StyleSheet } from 'react-native';
-import Stepper from "react-native-js-stepper";
+// TODO
+//import Stepper from "react-native-js-stepper";
 
 class StepperComponent extends React.Component {
     constructor(props) {
@@ -36,10 +37,11 @@ class StepperComponent extends React.Component {
         });
 
         return (
-            <Stepper style={{height: 10, margin: 0, padding: 0}}
+              <Text>Stepper component TODO</Text>
+/*            <Stepper style={{height: 10, margin: 0, padding: 0}}
             initialPage={this.props.index} steps={["Amount", "Billing", "Payment"]} validation={false} activeDotStyle={styles.activeDot} inactiveDotStyle={styles.inactiveDot} showTopStepper={true} showBottomStepper={false} activeStepStyle={styles.activeStep} inactiveStepStyle={styles.inactiveStep} activeStepTitleStyle={styles.activeStepTitle} inactiveStepTitleStyle={styles.inactiveStepTitle} activeStepNumberStyle={styles.activeStepNumber} inactiveStepNumberStyle={styles.inactiveStepNumber}>
                 <Text> hello</Text>
-            </Stepper>
+            </Stepper>*/
         );
         
     }
