baf2e43db82499db20c427e8a67fd83505f27eca
aivantg <aivant.developer@gmail.com>
2019-03-17 19:19:38 -0700

    slight formatting changes

--- app/lib/background.js
+++ app/lib/background.js
@@ -2,7 +2,50 @@ import BackgroundFetch from 'react-native-background-fetch';
 import { AsyncStorage } from 'react-native';
 import { notify } from './notifications';
 import { getRequest } from './requests';
-// import DeviceInfo from 'react-native-device-info';
+// import DeviceInfo from 'react-native-device-info'; Used to provide debug info
+
+/**
+ * Initializes Background Fetching when app loads.
+ * Also checks if this is the first app launch and initalizes
+ * The 'last updated' field for messages
+ */
+function initializeBackgroundFetch() {
+  BackgroundFetch.configure(
+    {
+      minimumFetchInterval: 60, // <-- minutes (15 is minimum allowed)
+      forceReload: true, // <-- App will reload....after it dies...eek..
+      stopOnTerminate: false, // <-- Fetching should continue when app terminates
+      startOnBoot: true, // <-- Fetching should restart when phone reboots
+    },
+    () => {
+      console.log('[js] Received background-fetch event');
+      backgroundTask();
+    },
+    (error) => {
+      console.log('[js] RNBackgroundFetch failed to start');
+      console.log(error);
+    },
+  );
+}
+
+// Function defining what to do when phone wakes up.
+// In this case, request new messages!
+// Idea: Send server the last message you received so that it
+// can know to only send you what you haven't seen.
+function backgroundTask() {
+  getRequest(
+    '/messages',
+    (newMessages) => {
+      processMessages(newMessages);
+    },
+    (error) => {
+      console.log('Request for messages failed');
+      console.log(error);
+      BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_FAILED);
+    },
+  );
+}
+
 
 // Function defining what to do with new messages
 async function processMessages(newMessages) {
@@ -50,48 +93,6 @@ async function processMessages(newMessages) {
     console.log(error);
     BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_FAILED);
   }
-}
-
-// Function defining what to do when phone wakes up.
-// In this case, request new messages!
-// Idea: Send server the last message you received so that it
-// can know to only send you what you haven't seen.
-function backgroundTask() {
-  getRequest(
-    '/messages',
-    (newMessages) => {
-      processMessages(newMessages);
-    },
-    (error) => {
-      console.log('Request for messages failed');
-      console.log(error);
-      BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_FAILED);
-    },
-  );
-}
-
-/**
- * Initializes Background Fetching when app loads.
- * Also checks if this is the first app launch and initalizes
- * The 'last updated' field for messages
- */
-function initializeBackgroundFetch() {
-  BackgroundFetch.configure(
-    {
-      minimumFetchInterval: 60, // <-- minutes (15 is minimum allowed)
-      forceReload: true, // <-- App will reload....after it dies...eek..
-      stopOnTerminate: false, // <-- Fetching should continue when app terminates
-      startOnBoot: true, // <-- Fetching should restart when phone reboots
-    },
-    () => {
-      console.log('[js] Received background-fetch event');
-      backgroundTask();
-    },
-    (error) => {
-      console.log('[js] RNBackgroundFetch failed to start');
-      console.log(error);
-    },
-  );
 }
 
 // Note: There exists a notification debug server that gets pinged when a
